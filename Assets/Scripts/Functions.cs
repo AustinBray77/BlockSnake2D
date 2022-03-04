0e0870cd58098ada32c838606d438df9fa9976b2
deoignition <93416177+AustinBray77@users.noreply.github.com>
2022-03-03 21:43:06 -0500

    Added Generic Functions

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
+using UnityEngine.Networking;
+using System;
 using System.Collections;
 
 public static class Functions
 {
+    public static DateTime currentTime { get; private set; }
+    public static bool fetchingDateTime { get; private set; }
+
+    public static U TryGenericConversion<T, U>(T val)
+    {
+        U output = default(U);
+
+        try
+        {
+            output = (U)Convert.ChangeType(val, typeof(U));
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Error Caught When Trying Generic Conversion: " + e.ToString());
+        }
+
+        return output;
+    }
+
     public static string ArrayToString<T>(T[] vals, string seperator = " ")
     {
         string output = "";
@@ -14,6 +35,9 @@ public static class Functions
 
         return output.Substring(0, output.Length - seperator.Length);
     }
+
+    public static T RandomArrItem<T>(T[] vals) =>
+        vals[UnityEngine.Random.Range(0, vals.Length)];
 
     public static bool PositionIsInBounds2D(Vector2 position, Vector2 topRight, Vector2 bottomLeft) =>
         position.x <= topRight.x && position.x >= bottomLeft.x && position.y <= topRight.y && position.y >= bottomLeft.y;
@@ -33,4 +57,50 @@ public static class Functions
 
     public static Vector3 CopyVector3(Vector3 vec) =>
         new Vector3(vec.x, vec.y, vec.z);
+
+    public static IEnumerator UpdateCurrentTime()
+    {
+        fetchingDateTime = true;
+
+        string url = "https://blcksnktime.000webhostapp.com/servertime.php";
+
+        UnityWebRequest webReq = UnityWebRequest.Get(url);
+
+        webReq.timeout = 3;
+
+        yield return webReq.SendWebRequest();
+
+        if (UnityWebRequest.Result.ConnectionError == webReq.result)
+        {
+            currentTime = new DateTimeOffset().DateTime;
+        }
+        else
+        {
+            string time = webReq.downloadHandler.text;
+
+            if (DateTime.TryParse(time, out DateTime serverTime))
+            {
+                currentTime = serverTime;
+            }
+        }
+
+        fetchingDateTime = false;
+    }
+
+    public static long CurrentTimeInMillis() =>
+        new DateTimeOffset(currentTime).ToUnixTimeMilliseconds();
+
+    public static long TimezoneOffset()
+    {
+        return ((System.DateTimeOffset.Now.ToUnixTimeMilliseconds() / 3600000) - (CurrentTimeInMillis() / 3600000)) % 24;
+    }
+
+    public static long DaysSinceUnixFromMillis(long millis) =>
+        millis / 86400000;
+
+    public static long CurrentMillisInTimeZone() =>
+        CurrentTimeInMillis() + (3600000 * TimezoneOffset());
+
+    public static float RoundToPlaces(float f, int places) =>
+        Mathf.Round(f * Mathf.Pow(10, places)) / Mathf.Pow(10, places);
 }
